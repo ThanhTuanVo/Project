649bf7d73cc506efba602b8b70827ea25c96cdf8
John Doe <sbd33th25@gmail.com>
2025-04-26 15:13:13 +0700

    moi

--- Slaveee/src/main.cpp
+++ Slaveee/src/main.cpp
@@ -3,14 +3,27 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <ArduinoJson.h>
+#include <Adafruit_MAX31865.h>
+#include <Adafruit_SHT31.h>
 
 // Định nghĩa các thông số cho Board
-#define BOARD_ID 1
-#define DHTPIN 4
-#define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
+#define MAX31865_CS  12
+#define MAX31865_SDI 10
+#define MAX31865_SDO 11
+#define MAX31865_CLK 16
+#define SDA_PIN 36
+#define SCL_PIN 35
 
+#define BOARD_ID 1
 HardwareSerial mySerial(1);
+
+#define RREF      430.0
+#define RNOMINAL  100.0
+
+Adafruit_MAX31865 pt100 = Adafruit_MAX31865(MAX31865_CS, MAX31865_SDI, MAX31865_SDO, MAX31865_CLK);
+
+// ----------------- Cảm biến SHT3x -------------------
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 // Cấu trúc dữ liệu cảm biến và tham số
 typedef struct sensor {
@@ -19,7 +32,7 @@ typedef struct sensor {
     float hum;
     int readingId;
 } sensor;
-sensor DHTsensor;
+sensor PTaSHTsensor;
 
 typedef struct parameter {
   float temp1, temp2, temp3, temp4;
@@ -27,6 +40,9 @@ typedef struct parameter {
   String time1, time2, time3, time4;
 } parameter;
 parameter FVparameter;
+
+bool StartState = false;
+
 
 // Hàm nhận dữ liệu từ Master qua UART
 void receiveDataFromMaster(void *parameter) {
@@ -43,6 +59,7 @@ void receiveDataFromMaster(void *parameter) {
       if (!error) {
         String type = doc["type"].as<String>();
         if (type == "parameter") {
+          
           JsonObject stage1 = doc["Say khu am"];
           FVparameter.temp1 = stage1["temp"];
           FVparameter.hum1 = stage1["hum"];
@@ -72,6 +89,17 @@ void receiveDataFromMaster(void *parameter) {
           Serial.printf("On dinh - Temp: %.2f°C, Hum: %.2f%%, Time: %s\n", FVparameter.temp3, FVparameter.hum3, FVparameter.time3.c_str());
           Serial.printf("Bao quan - Temp: %.2f°C, Hum: %.2f%%, Time: %s\n", FVparameter.temp4, FVparameter.hum4, FVparameter.time4.c_str());
         }
+
+        if (type == "StartState"){
+          StartState = doc["Start"];
+          if (StartState) {
+            Serial.println("Start state received: true");
+            // gọi chương trình hoạt động
+          } else {
+            Serial.println("Start state received: false");
+          }
+
+        }
         Serial.println();
       } else {
         Serial.println("Unknown type received.");
@@ -85,27 +113,31 @@ void receiveDataFromMaster(void *parameter) {
 void sendSensorDataToMaster(void *parameter) {
   while (true) {
     // Đọc dữ liệu từ cảm biến trong task này
-    DHTsensor.id = BOARD_ID;
-    DHTsensor.readingId++;
+    PTaSHTsensor.id = BOARD_ID;
+    PTaSHTsensor.readingId++;
 
-    DHTsensor.temp = dht.readTemperature();
-    DHTsensor.hum = dht.readHumidity();
+    float pt100_temp = pt100.temperature(RNOMINAL, RREF);
+    float sht_hum = sht31.readHumidity();
 
-    if (isnan(DHTsensor.temp) || isnan(DHTsensor.hum)) {
-      Serial.println("Failed to read from DHT sensor!");
-      DHTsensor.temp = 0;
-      DHTsensor.hum = 0;
-    } else {
-      Serial.println("Temp: " + String(DHTsensor.temp) + "°C, Hum: " + String(DHTsensor.hum) + "%");
+    if (isnan(pt100_temp)) {
+      Serial.println("Lỗi đọc PT100!");
+      pt100_temp = 0;
     }
+    if (isnan(sht_hum)) {
+      Serial.println("Lỗi đọc SHT31!");
+      sht_hum = 0;
+    }
+
+    PTaSHTsensor.temp = pt100_temp;
+    PTaSHTsensor.hum = sht_hum;
 
     // Tạo JSON cho dữ liệu cảm biến DHT
     StaticJsonDocument<200> doc;
     doc["type"] = "sensor";
-    doc["id"] = DHTsensor.id;
-    doc["temp"] = DHTsensor.temp;
-    doc["hum"] = DHTsensor.hum;
-    doc["readingId"] = DHTsensor.readingId;
+    doc["id"] = PTaSHTsensor.id;
+    doc["temp"] = PTaSHTsensor.temp;
+    doc["hum"] = PTaSHTsensor.hum;
+    doc["readingId"] = PTaSHTsensor.readingId;
 
     String output;
     serializeJson(doc, output);
@@ -119,10 +151,19 @@ void sendSensorDataToMaster(void *parameter) {
 }
 
 void setup() {
+  Wire.begin(36, 35); // SDA: GPIO 48, SCL: GPIO 47
   Serial.begin(115200);
   mySerial.begin(115200, SERIAL_8N1, 18, 17); // RX: GPIO 18, TX: GPIO 17
-  dht.begin();
 
+  if (!pt100.begin(MAX31865_3WIRE)) {
+    Serial.println(" Không tìm thấy cảm biến PT100!");
+    while (1) delay(1);
+  }
+
+  if (!sht31.begin(0x44)) {
+    Serial.println(" Không tìm thấy cảm biến SHT3x!");
+    while (1) delay(1);
+  }
   // Tạo task để nhận dữ liệu từ Master
   xTaskCreate(receiveDataFromMaster, "ReceiveData", 4096, NULL, 1, NULL);
 
